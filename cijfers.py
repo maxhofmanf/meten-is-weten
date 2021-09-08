0f4e5c457ba3e6a5b82a0150c85afbb4b4396b35
maxhofmanf <99068281@mydavinci.nl>
2021-09-08 10:48:09 +0100

    else-statement

---
+++ cijfers.py
@@ -0,0 +1,8 @@
+a = 60
+b = int(input('kies een cijfer: '))
+if a > b:
+    print('A is het grootste getal')
+elif a < b:
+    print('A is het kleinste getal')
+else:
+    print('A en B zijn even groot')
